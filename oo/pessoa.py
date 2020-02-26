e4994869cca8250fe0e8e6ea16eef41178d70762
Fabiano <fabianosfigueiredo@gmail.com>
2020-02-26 00:45:17 -0300

    criando método de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,6 +7,14 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
 if __name__ == '__main__':
     renzo = Pessoa(nome='Renzo')
     luciano = Pessoa(renzo, nome='Fabiano')
@@ -23,4 +31,12 @@ if __name__ == '__main__':
 
     print(luciano.__dict__)
     print(renzo.__dict__)
+
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(renzo.olhos)
+    print(id(Pessoa.olhos),id(luciano.olhos),id(renzo.olhos))
+    print(Pessoa.metodo_estatico(),luciano.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(),luciano.nome_e_atributos_de_classe())
 
